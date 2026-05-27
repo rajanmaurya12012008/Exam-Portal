04bc723c0e27c776347bf7aba1f520d887e02b94
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-05-27 12:18:04 +0530

    changes some quetions

--- que.py
+++ que.py
@@ -2,6 +2,6 @@ questions = [
     {"question": "5 + 3", "answer": 8},
     {"question": "19 - 9", "answer": 10},
     {"question": "6 * 12", "answer": 72},
-    {"question": "9 + 11", "answer": 20},
+    {"question": "11 + 11", "answer": 22},
     {"question": "7 - 2", "answer": 5}
 ]
