3185e0dd60195b6ac93dd13d31bd3a4f9c2889c4
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-05-27 12:23:37 +0530

    some updates

--- que.py
+++ que.py
@@ -3,5 +3,5 @@ questions = [
     {"question": "19 - 9", "answer": 10},
     {"question": "6 * 12", "answer": 72},
     {"question": "11 + 11", "answer": 22},
-    {"question": "7 - 2", "answer": 5}
+    {"question": "17 - 15", "answer": 2}
 ]
